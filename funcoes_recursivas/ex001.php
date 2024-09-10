01f92495c8a06246ff73c4290195d3d753ef5b62
Italo Cruz <italocruz544@gmail.com>
2024-09-10 17:08:20 -0300

    feat: add new subject

--- funcoes_recursivas/ex001.php
+++ funcoes_recursivas/ex001.php
@@ -5,16 +5,44 @@
             'subordinados' => array(
                 // Inicio: Diretor comercial
                 array(
-                    'nome_cargo' => 'Diretor Comercial'
+                    'nome_cargo' => 'Diretor Comercial',
                     'subordinados' => array(
+                        // Inicio: Gerente de Vendas
                         array(
                             'nome_cargo' => 'gerente de Vendas'
                         )
                     )
                 ),
-
-                array()
+                // Inicio: Diretor Financeiro
+                array(
+                    'nome_cargo' => 'Diretor Financeiro',
+                    'Subordinados' => array(
+                        // Inicio gerente de Contas
+                        'nome_cargo' => 'Gerente de contas',
+                    )
+                )
             )
         )
     );
+
+    function exibe($cargos){
+        $html = '<ul>';
+            foreach($cargos as $cargo){
+                $html .= "<li>";
+
+                $html .= $cargo['nome_cargo'];
+
+                if(isset($cargo['subordinados']) && count($cargo['subordinados']) > 0){
+                    $html .= exibe($cargo['subordinados']);
+                }
+
+                $html .= "</li>";
+            }
+        $html .= "</ul>";
+
+        return $html;
+
+    }
+
+    echo exibe($hierarquia);
 ?>
